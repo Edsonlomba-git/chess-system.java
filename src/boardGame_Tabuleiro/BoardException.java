45879b4ea143593f29dd1209af1e3a9b88c55d21
Edson Lomba <educacaomoderna5d@gmail.com>
2020-09-09 23:29:25 -0300

    BoardException and defensive programaming

---
+++ src/boardGame_Tabuleiro/BoardException.java
@@ -0,0 +1,10 @@
+package boardGame_Tabuleiro;
+
+public class BoardException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public BoardException(String msg){
+		super(msg);
+	}
+}
