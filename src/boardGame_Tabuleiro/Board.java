81c063240c922990f14a3622572969d48f0d4188
Edson Lomba <educacaomoderna5d@gmail.com>
2020-09-09 22:22:31 -0300

    placing pieces on the board

--- src/boardGame_Tabuleiro/Board.java
+++ src/boardGame_Tabuleiro/Board.java
@@ -4,15 +4,15 @@ public class Board {
 	private Integer rows;
 	private int columns;
 	private Piece[][] pieces;
-	
-	/**********************	Constructors	******************************/
+
+	/********************** Constructors ******************************/
 	public Board(Integer rows, int columns) {
 		this.rows = rows;
 		this.columns = columns;
 		this.pieces = new Piece[rows][columns];
 	}
-	
-	/**********************	Encapsulation	******************************/
+
+	/********************** Encapsulation ******************************/
 	public Integer getRows() {
 		return rows;
 	}
@@ -28,11 +28,19 @@ public class Board {
 	public void setColumns(int columns) {
 		this.columns = columns;
 	}
+
 	/*********************************************************************/
 	public Piece piece(int row, int column) {
 		return pieces[row][column];
 	}
+
 	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getColumn()];
+	}
+
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+
+		piece.position = position;
 	}
 }
